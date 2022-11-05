1fc2a8eb3af6168c568f18ab3f98269f23d83fa1
Vasavivarsha1709 <111517812+Vasavivarsha1709@users.noreply.github.com>
2022-11-05 23:11:40 +0530

    Create JoptionPane.java

---
+++ src/train/JoptionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package train;
+
+/**
+ *
+ * @author Vasavi Varsha
+ */
+class JoptionPane {
+
+    static void showMessageDialog(reservation aThis, String record_Addedddd) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
